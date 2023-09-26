eeffa24add30cacc4e4caa13d48ec01b9764e184
Travis Gerke <tgerke@mail.harvard.edu>
2023-09-26 16:39:53 -0700

    fix rendering script

--- ghgenerate.R
+++ ghgenerate.R
@@ -1,6 +1,6 @@
-proposal<-"Boilerplate ISC Proposal"
+proposal<-"Causal inference in a Box"
 proposal.file<-"isc-proposal.Rmd"
-author<-"Steph Locke"
+author<-"Lucy D’Agostino McGowan, Malcolm Barrett, Travis Gerke"
 
 rmarkdown::render(proposal.file, output_format="html_document",
                   output_dir="out", quiet=TRUE)
